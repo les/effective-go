05fed7312f61c66d8a43c9b55eab5fe1cb50c878
İnanç Gümüş <m@inanc.io>
2023-04-29 11:25:44 +0300

    Add listing 7.14

---
+++ ch07/httpio/log.go
@@ -0,0 +1,16 @@
+package httpio
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		end := time.Since(start)
+		log.Printf("%s %s %s %v", r.Method, r.URL.Path, r.RemoteAddr, end)
+	})
+}
