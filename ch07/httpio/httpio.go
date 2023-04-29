9969154ae69debef832f693b0bc56e9bab110abc
İnanç Gümüş <m@inanc.io>
2023-04-29 11:36:14 +0300

    Add listing 7.21

--- ch07/httpio/httpio.go
+++ ch07/httpio/httpio.go
@@ -19,12 +19,26 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // Error returns an httpio.Handler that writes the given error message
 // to the response. If the error is internal, it logs it and hides the
 // actual error message from the client.
-func Error(code int, message string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func Error(code int, message string) Handler {
+	return func(w http.ResponseWriter, r *http.Request) http.Handler {
 		if code == http.StatusInternalServerError {
 			Log(r.Context(), "%s: %v", r.URL.Path, message)
 			message = bite.ErrInternal.Error()
 		}
-		http.Error(w, message, code)
+		return JSON(code, map[string]string{
+			"error": message,
+		})
+	}
+}
+
+// JSON returns an httpio.Handler that writes the given value to the
+// response as JSON. If the value cannot be encoded, it logs the error
+// and returns an internal error to the client.
+func JSON(code int, v any) Handler {
+	return func(w http.ResponseWriter, r *http.Request) http.Handler {
+		if err := Encode(w, code, v); err != nil {
+			Log(r.Context(), "%s: encoding to JSON: %v", r.URL.Path, err)
+		}
+		return nil
 	}
 }
