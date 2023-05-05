44328e0e333df18f28925fb2d0354874fac59887
İnanç Gümüş <m@inanc.io>
2023-05-05 21:33:33 +0300

    Add listing 8.2

--- ch08/sqlx/sqlx.go
+++ ch08/sqlx/sqlx.go
@@ -10,5 +10,32 @@
 package sqlx
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+
 	_ "modernc.org/sqlite"
 )
+
+// DefaultDriver is the default SQL driver.
+const DefaultDriver = "sqlite"
+
+// DB is a wrapper around database/sql.DB.
+type DB struct {
+	*sql.DB
+}
+
+// Dial is like sql.Open + sql.Ping and also migrates the schema.
+func Dial(ctx context.Context, driver, dsn string) (*DB, error) {
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("opening database driver %q by %q: %w", driver, dsn, err)
+	}
+	// actually it wasn't necessary to ping the database
+	// as ExecContext will do it for us. however, it made
+	// the book explanation easier :-)
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("pinging database: %w", err)
+	}
+	return &DB{DB: db}, nil
+}
