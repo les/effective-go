8b835d04f58185ec457dc26acdb77a9118795485
İnanç Gümüş <m@inanc.io>
2023-05-06 15:56:11 +0300

    Add listing 8.21

--- ch08/cmd/shortd/shortd.go
+++ ch08/cmd/shortd/shortd.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"log"
@@ -9,20 +10,35 @@ import (
 	"time"
 
 	"github.com/inancgumus/effective-go/ch08/httpio"
+	"github.com/inancgumus/effective-go/ch08/short"
 	"github.com/inancgumus/effective-go/ch08/shortener"
+	"github.com/inancgumus/effective-go/ch08/sqlx"
 )
 
 func main() {
 	var (
 		addr    = flag.String("addr", "localhost:8080", "server address")
 		timeout = flag.Duration("timeout", 10*time.Second, "server timeout per request")
+		dns     = flag.String("db", "file:bite.db?mode=rwc", "database connection string")
 	)
 	flag.Parse()
 
 	logger := log.New(os.Stderr, "shortener: ", log.LstdFlags|log.Lmsgprefix)
 	logger.Println("starting the server on", *addr)
 
-	shortenerServer := &shortener.Server{}
+	db, err := sqlx.Dial(context.Background(), sqlx.DefaultDriver, *dns)
+	if err != nil {
+		logger.Println("connecting to database:", err)
+		return
+	}
+	svc := &shortener.Service{
+		LinkStore: &short.LinkStore{
+			DB: db,
+		},
+	}
+	shortenerServer := &shortener.Server{
+		Service: svc,
+	}
 	shortenerServer.RegisterRoutes()
 
 	server := &http.Server{
@@ -34,8 +50,7 @@ func main() {
 		server.ErrorLog = logger
 		server.Handler = httpio.LoggingMiddleware(server.Handler)
 	}
-	err := server.ListenAndServe()
-	if !errors.Is(err, http.ErrServerClosed) {
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		logger.Println("server closed unexpectedly:", err)
 	}
 }
