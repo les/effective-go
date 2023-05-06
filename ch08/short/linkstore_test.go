e8f6ba3391af83c0db644301297078c447627837
İnanç Gümüş <m@inanc.io>
2023-05-06 12:27:10 +0300

    Add listing 8.10

--- ch08/short/linkstore_test.go
+++ ch08/short/linkstore_test.go
@@ -2,8 +2,10 @@ package short
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/inancgumus/effective-go/ch08/bite"
 	"github.com/inancgumus/effective-go/ch08/sqlx/sqlxtest"
 )
 
@@ -21,6 +23,12 @@ func TestLinkStore(t *testing.T) {
 		err := store.Create(ctx, link)
 		if err != nil {
 			t.Errorf("Create() err = %q, want <nil>", err)
+		}
+	})
+	t.Run("create/exists", func(t *testing.T) {
+		err := store.Create(ctx, link)
+		if !errors.Is(err, bite.ErrExists) {
+			t.Errorf("Create(%q) err = %q, want %q", link.Key, err, bite.ErrExists)
 		}
 	})
 }
